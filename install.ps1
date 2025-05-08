30a7daee4f63507e9bd37a8c5058fa2857359504
wuKa <wuka.sec@gmail.com>
2025-05-08 15:06:00 +0330

    Add Warp Installation

--- install.ps1
+++ install.ps1
@@ -7,6 +7,7 @@ winget install neovim --id Neovim.Neovim -s winget
 winget install JanDeDobbeleer.OhMyPosh -s winget
 winget install ntop --id gsass1.NTop -s winget
 winget install wget --id JernejSimoncic.Wget
+winget install Warp.Warp
 
 if (-Not (Get-Command scoop -ErrorAction SilentlyContinue)) {
     Write-Host "🔍 Scoop is NOT installed. Installing now..." -ForegroundColor Yellow
