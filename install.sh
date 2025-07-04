0dc222ca9c22b6c84e3900d60324e61732614d50
wuKa <wuka.sec@gmail.com>
2025-07-04 20:55:39 +0330

    Add Basic Installer for Linux

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+
+sudo apt install -y obs-studio neovim htop lsd bat gh zsh fish vlc 
+echo -e "\n\n\t -> Install Telegram, V2rayNg & Code Editor (vscode) manually"
+echo -e "\n\n\t -> Config Telegram, V2rayNg Code Editor (vscode), gh, zsh and neovim"
+
