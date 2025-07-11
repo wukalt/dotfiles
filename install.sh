9842d451c9064c504e48d6e8dac0b3ead11c8458
wuKa <wuka.sec@gmail.com>
2025-07-11 18:49:21 -0500

    Update install.sh
    yeah ...

--- install.sh
+++ install.sh
@@ -2,7 +2,16 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 
-sudo apt install -y obs-studio neovim htop lsd bat gh zsh fish vlc 
+sudo apt install -y obs-studio vim htop lsd bat gh zsh fish vlc python3-pip \
+  fonts-firacode git v2ray traceroute whois wireshark net-tools virtualbox virtualbox-ext-pack docker.io \
+  micro 
+
+pip3 install requests --break-system-packages 
+pip3 install django --break-system-packages
+
+chsh -s $(which fish)
+sudo chsh -s $(which fish)
+
 echo -e "\n\n\t -> Install Telegram, V2rayNg & Code Editor (vscode) manually"
-echo -e "\n\n\t -> Config Telegram, V2rayNg Code Editor (vscode), gh, zsh and neovim"
+echo -e "\n\n\t -> Config telegram, v2ray, gh, zsh"
 
