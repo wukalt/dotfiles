c7a1ac76adc509913df97871817bebcbaab83f65
Mehdi Mohseni <wuka.sec@gmail.com>
2025-07-15 00:25:15 +0330

    Update: install.sh, README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PowerShellConfig
+# My Setup
 
 ## ⚠ Backup Warning
 
@@ -7,31 +7,36 @@ If your configuration files exist, this script may **modify or delete** them..
 Always have a **recovery plan** before making changes.
 
 ### 🔥 Files That May Be Removed or Replaced 
-The following files **could be deleted or replaced**, so ensure you have a backup:
+The following files **could be deleted or replaced**, so ensure you have a backup (WINDOWS Devices):
 - `C:\Users\YourUsername\.config\`
 - `C:\Users\YourUsername\.config\powershell`
-- `C:\Users\YourUsername\AppData\nvim`
 - `C:\Users\YourUsername\AppData\nvim\*`
 - `$PROFILE`
 - `$PROFILE.CurrentUserCurrentHost`
 
-**💡 Tip:** To understand how this script works, examine the code. The primary file responsible for modifying files is **`install.ps1`**, so be sure to check the source.
+and for **Linux:**
+- `~/.vimrc`
+- `~/.zshrc`
+- some **python packages** will installed (if you installed them so **upgraded**)
+
+
+**💡 Tip:** To understand how this script works, examine the code. The primary file responsible for modifying files is **`install.ps1`** or **`install.sh`**, so be sure to check the source.
 
 # Setup
 
-1. Download a **Nerd Font** from here: https://www.nerdfonts.com/font-downloads.
+1. Download a **Nerd Font** from [here](https://www.nerdfonts.com/font-downloads) (if on windows)
 
-2. Install **Windows-Terminal** from Microsoft app store.
+2. Install **Windows-Terminal** from Microsoft app store.(if on windows)
 
-3. Install **Winget**. 
+3. Install **Winget**. (if on windows)
 
 4. Clone The Proj : 
     ```bash
-    git clone https://github.com/wukalt/PowerShellConfig.git
+    git clone https://github.com/wukalt/MySetup.git
     ```
 5. Change Directory : 
     ```bash
-    cd PowerShellConfig
+    cd MySetup
     ```
 
 6. Run Installer : 
@@ -40,13 +45,14 @@ The following files **could be deleted or replaced**, so ensure you have a backu
     bash install.sh   # for Linux 
     ```
 
+
 ## 🙌 Thanks To
 
 - **FreeCodeCamp.org** for  vim config file 
     
-    link: https://www.freecodecamp.org/news/vimrc-configuration-guide-customize-your-vim-editor/
+    link: [vimrc configuration guide customize your vim](https://www.freecodecamp.org/news/vimrc-configuration-guide-customize-your-vim-editor)
 
 
 - **Scott Hanselman** for oh-my-posh config file 
 
-    link: https://www.hanselman.com/blog/my-ultimate-powershell-prompt-with-oh-my-posh-and-the-windows-terminal/
+    link: [my ultimate powershell prompt with oh-my-posh](https://www.hanselman.com/blog/my-ultimate-powershell-prompt-with-oh-my-posh-and-the-windows-terminal)
--- install.sh
+++ install.sh
@@ -1,51 +1,79 @@
 #!/bin/bash
 
-# Update & Upgrade
+
+# == VARS ==
+GREEN='\033[0;32m'
+
+
+# == Update & Upgrade ==
+echo -e "\t\n updating, upgrading and removing old apps ...\n\n"
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt autoremove -y
+echo -e "${GREEN}\t\n Successful...\n\n"
 
-# Installing apps using APT
+
+# == Installing apps using APT ==
+echo -e "\t\n Installing apps using APT package manager ...\n\n"
 sudo apt install -y obs-studio vim htop lsd bat gh zsh fish vlc python3-pip \
   fonts-firacode git v2ray traceroute whois wireshark net-tools virtualbox docker.io \
   micro wine wine32 wine64 wine64-tools iw wifite aircrack-ng bully hashcat hcxdumptool \
-  hcxtools macchanger nmap curl wget tmux pipenv
+  hcxtools macchanger nmap curl wget tmux pipenv ipython3 
+echo -e "${GREEN}\t\n Successful...\n\n"
 
-# Installing apps using snap
+
+# == Installing apps using snap ==
+echo -e "\t\n Installing apps using Snap...\n\n"
 sudo snap install telegram-desktop
-sudo snap install v2raya
 sudo snap install code --classic
+echo -e "${GREEN}\t\n Successful...\n\n"
 
-# Install python modules using pip
+
+# == Install python modules using pip ==
+echo -e "\t\n Installing python modules...\n\n"
 pip3 install django --break-system-packages
 pip3 install requests --break-system-packages 
 pip3 install numpy --break-system-packages
 pip3 install pandas --break-system-packages  
 pip3 install pipenv --break-system-packages  
+pip3 install pytest --break-system-packages
+echo -e "${GREEN}\t\n Successful...\n\n"
+
 
 # Configs
+echo "\t\n Configure fish...\n\n"
 chsh -s $(which fish)
 sudo chsh -s $(which fish)
+echo -e "${GREEN}\t\n Successful...\n\n"
 
-  # ZSH Config
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestionsgit 
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-  echo -e "set -> ZSH_THEME="powerlevel10k/powerlevel10k"
-  echo -e "set -> plugins=(git zsh-autosuggestions zsh-syntax-highlighting)"
 
-  echo "alias ll=\"lsd -l\"" >> ~/.zshrc
-  echo "alias g=\"git\"" >> ~/.zshrc
-  echo "alias cat=\"batcat\"" >> ~/.zshrc
+# ZSH Config
+echo "\t\n Configure zsh...\n\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestionsgit 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-  # VIM Config
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  cp vim_config.vim ~/.vimrc
-  
-  echo "run -> vim :PlugInstall"
+sed -i "/ZSH_THEME=/d" ~/.zshrc
+sed -i "plugins=(" ~/.zshrc
+
+echo "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" >> ~/.zshrc
+echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting)" >> ~/.zshrc
+
+echo "alias ll=\"lsd -l\"" >> ~/.zshrc
+echo "alias g=\"git\"" >> ~/.zshrc
+echo "alias cat=\"batcat\"" >> ~/.zshrc
+echo -e "${GREEN}\t\n Successful...\n\n"
+
+
+# VIM Config
+echo "\t\n Configure vim...\n\n"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp vim_config.vim ~/.vimrc
+
+echo -e "${GREEN}\t\n Successful...\n\n"
+echo "run -> vim :PlugInstall"
 
 # Checkout and show dialog
-echo -e "\t -> Install vscode manually"
-echo -e "\t -> Config telegram, v2ray, gh, zsh"
-
+echo -e "\t -> Config telegram, v2ray, gh"
+echo -e "${GREEN}\t\n Done.............\n\n"
